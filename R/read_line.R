b9d69131e9622cca35d4008cddd880d5c4ec587c
Jen-Hsiang Ou <oumark.me@outlook.com>
2023-09-05 16:12:35 +0200

    stable version

--- R/read_line.R
+++ R/read_line.R
@@ -14,36 +14,18 @@
 #' @export
 read_line = function(VCF, chr=NULL, pos=NULL, infoOnly=TRUE){
 
-  # return dat
-  dat = NULL
-
   # check if index file exist with the VCF file
   if(! file.exists(paste(VCF, "tbi", sep="."))){
     cat("Index file not found. Generate index file for random reading.")
     system(paste("tabix -p vcf", VCF))
   }
 
-  # Header
-  header = system(paste('tabix -H', VCF, "| grep '#CHROM'"), intern = T)
-  header = strsplit(header, "\t")[[1]]
-  header[1] = "CHROM"
-
-  # If only read variant information
-  if(infoOnly){
-    infoOnly = 1:9
-    header = header[infoOnly]
-  }else{
-    infoOnly = NULL
-  }
-
+  # Read line
   SCRIPT = paste("tabix", VCF, paste0(chr, ":", pos, "-", pos))
   dat = str_split(system(SCRIPT, intern = TRUE), "\t", simplify = TRUE)
-  if(! is.null(infoOnly)){
-    dat = dat[infoOnly]
-  }
-  dat = as.data.frame(dat)
-  colnames(dat) = header
 
+  # If only read variant information
+  if(infoOnly) dat = dat[, 1:9]
 
   # return result
   return(dat)
